268d798506d787d537b616910da4b90844164f7c
jacobsfootmib-ux <jacobsfoot.mib@gmail.com>
2025-09-23 23:49:48 -0700

    Update upperdeck-epack-list-export.user.js

--- upperdeck-epack-list-export.user.js
+++ upperdeck-epack-list-export.user.js
@@ -6,18 +6,8 @@
 // @match        https://www.upperdeckepack.com/*
 // @run-at       document-idle
 // @grant        none
-// @updateURL    https://raw.githubusercontent.com/<your-username>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
-// @downloadURL  https://raw.githubusercontent.com/<your-username>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
-// ==/UserScript==
-
-// ==UserScript==
-// @name         Upper Deck e-Pack: List-View CSV Export (Group Parser)
-// @namespace    epack-export
-// @version      1.0.0
-// @description  Adds an 'Export ePack CSV' button that scrapes list-view groups (.group), scrolls to load all, and downloads a CSV.
-// @match        https://www.upperdeckepack.com/*
-// @run-at       document-idle
-// @grant        none
+// @updateURL    https://raw.githubusercontent.com/<jacobsfootmib-ux>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
+// @downloadURL  https://raw.githubusercontent.com/<jacobsfootmib-ux>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
 // ==/UserScript==
 
 (function () {
