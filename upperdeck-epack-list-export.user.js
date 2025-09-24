60a2f89b4ac9e291fe6ce2b81e9f020057356515
jacobsfootmib-ux <jacobsfoot.mib@gmail.com>
2025-09-23 23:45:53 -0700

    Add Tampermonkey export script v1.0.0

---
+++ upperdeck-epack-list-export.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Upper Deck e-Pack: List-View CSV Export (Group Parser)
+// @namespace    https://github.com/<jacobsfootmib-ux>/upperdeck-epack-export
+// @version      1.0.0
+// @description  Export e-Pack collection from List view (.group) to CSV
+// @match        https://www.upperdeckepack.com/*
+// @run-at       document-idle
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/<your-username>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
+// @downloadURL  https://raw.githubusercontent.com/<your-username>/upperdeck-epack-export/main/upperdeck-epack-list-export.user.js
+// ==/UserScript==
+
+// (your script body goes here)
